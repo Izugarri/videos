92f6a2e064c6c7410ada89e460bbfe7465836245
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-02-28 21:10:09 +0100

    nODE PARA rENDER

--- server.js
+++ server.js
@@ -3,7 +3,5 @@ const app = express();
 
 app.use(express.static(__dirname+'/'));
 
-console.log('escuchando en el puerto 15478, 15494 y 8080');
-app.listen(15494)
-app.listen(15478)
+console.log('escuchando en el puerto 8080');
 app.listen(8080);
