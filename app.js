d7ae82379a39ca831eb746e25ff875166e585b8f
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-08-21 17:26:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,23 +6,11 @@ const app = express()
 // Logs all request paths and method
 app.use(function (req, res, next) {
   res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
   console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
   next();
 });
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
+app.use(express.static(__dirname+'/'))
 
 // #############################################################################
 // Catch all handler for all other request.
