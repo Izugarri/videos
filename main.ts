3aa46ad88fd7291d38d6b4d1402312bb98b3a43b
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-30 21:33:47 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
-import { Application } from 'https://deno.land/x/abc@v1.0.2/mod.ts'
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-const app = new Application()
+const app = new Application();
+app.use(async (ctx) => {
+  try {
+    await ctx.send({
+      root: `${Deno.cwd()}`,
+      index: "index.html",
+    });
+  } catch {
+    ctx.response.status = 404;
+    ctx.response.body = "404 File not found";
+  }
+});
 
-var localPort = 80
-if (Deno.args.length > 0) {
-  localPort = parseInt(Deno.args[0])
-}
-app.static('/', '.').file('/', 'index.html').start({ port: localPort })
+await app.listen({ port: 8000 });
