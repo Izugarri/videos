2f59f66dc7e8f8ff8f0095e7a819e90d173651cb
izugarri3 <videomusicajmc@gmail.com>
2026-03-08 10:54:35 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,7 @@ import { serveDir } from "jsr:@std/http/file-server";
 
 Deno.serve((req) => {
   return serveDir(req, {
-    fsRoot: "public", // Carpeta que contiene los archivos
-    showDirListing: true, {
-    headers: {
-      // Browser caches for 60s
-      "Cache-Control": "public, max-age=5",
-      // Deno Deploy CDN caches for 1 hour
-      "Deno-CDN-Cache-Control": "public, s-maxage=6",
-    },
-  }
+    fsRoot: "./", // Carpeta que contiene los archivos
+    showDirListing: true,
   });
 });
