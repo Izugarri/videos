3fd6a6abcf1213799a7cf4f4951de4f0b610e9d3
izugarri3 <videomusicajmc@gmail.com>
2026-03-08 10:50:51 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,6 @@ Deno.serve((req) => {
     showDirListing: true, {
     headers: {
       "Cache-Control": "public, s-maxage=5", "Deno-CDN-Cache-Control": "public, s-maxage=6",
-    },
+    },}
   });
 });
